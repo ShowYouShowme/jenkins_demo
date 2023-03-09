a6d52bb0b2112b6d329af312e77abbc144f60746
wzc <wzc_0618@126.com>
2023-03-09 20:28:51 +0800

    实现测试接口

---
+++ index.ts
@@ -0,0 +1,29 @@
+import express = require('express');
+import bodyParser = require("body-parser");
+const port = 5000;
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res)=>{
+    res.send('hello world!');
+});
+
+app.get('/api/getUsers', (req, res)=>{
+    res.json([
+        {
+            name : 'terry',
+            age  : 25
+        },
+        {
+            name : 'marry',
+            age  : 18
+        }
+    ])
+});
+
+
+
+app.listen(port, '0.0.0.0', ()=>{
+    console.log(`Example app listening on port ${port}`);
+});
